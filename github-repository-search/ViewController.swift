649d8493c51de57b046fb3f4c8d3213cd49d3ddd
cocona15531 <issei.ueda@zozo.com>
2026-06-18 16:42:57 +0900

    Buttonを追加

--- github-repository-search/ViewController.swift
+++ github-repository-search/ViewController.swift
@@ -9,12 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let getButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("GET", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 18)
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 12
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .blue
     }
-
-
 }
 
