42a5b4719a9c8d2a9c51c61c8d47b1f4aabf1e92
cocona15531 <issei.ueda@zozo.com>
2026-06-25 12:21:02 +0900

    クエリパラメータを追加

--- github-repository-search/APIClient.swift
+++ github-repository-search/APIClient.swift
@@ -15,6 +15,10 @@ final class APIClient {
         // 1. URLComponents でベースURLを定義する。
         var components = URLComponents(string: "https://api.github.com/search/repositories")
 
+        // 2. クエリパラメータを追加する。
+        components?.queryItems = [
+            URLQueryItem(name: "q", value: "Swift"),
+        ]
         return nil
     }
 }
