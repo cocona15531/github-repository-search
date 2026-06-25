44f432165fb119c5d1130547b2850df94dbffefc
cocona15531 <issei.ueda@zozo.com>
2026-06-25 21:02:44 +0900

    URLRequestにヘッダーを追加

--- github-repository-search/APIClient.swift
+++ github-repository-search/APIClient.swift
@@ -31,6 +31,14 @@ final class APIClient {
         // GET メソッドを使用するので、httpMethod を "GET" に設定する。
         request.httpMethod = "GET"
 
+        // 5. application/vnd.github+json を Accept ヘッダーに設定することが公式ドキュメントで推奨されているので、設定する。
+        // https://docs.github.com/ja/rest/search/search?apiVersion=2026-03-10#search-repositories
+        request.setValue("application/vnd.github+json", forHTTPHeaderField: "Accept")
+
+        // 設定は推奨されていないが、コードサンプルでは X-GitHub-Api-Version ヘッダーを設定しており、
+        // APIのバージョンを指定することで将来的な互換性の問題を回避できる可能性があるため、設定しておく。
+        request.setValue("2026-03-10", forHTTPHeaderField: "X-GitHub-Api-Version")
+
         // 5. 作成した URLRequest を返す。
         return request
     }
