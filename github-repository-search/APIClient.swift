09923dd7a98e26d288b6be01ba989bbe5dfe99b5
cocona15531 <issei.ueda@zozo.com>
2026-06-25 12:30:21 +0900

    URLComponentsからURLを抽出するよう追加

--- github-repository-search/APIClient.swift
+++ github-repository-search/APIClient.swift
@@ -9,7 +9,7 @@ import Foundation
 
 final class APIClient {
 
-    /// URLRequestを作成する。
+    /// URLRequest を作成する。
     private func makeRequest() -> URLRequest? {
 
         // 1. URLComponents でベースURLを定義する。
@@ -19,6 +19,12 @@ final class APIClient {
         components?.queryItems = [
             URLQueryItem(name: "q", value: "Swift"),
         ]
+
+        // 3. URLComponents から URL を取得する。
+        // 正常に URL が作成できなかった場合は nil を返す。
+        guard let url = components?.url else {
+            return nil
+        }
         return nil
     }
 }
