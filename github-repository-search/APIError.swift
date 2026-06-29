1731b1a6638edab3e12a658eb426ea2e5d2b316f
cocona15531 <issei.ueda@zozo.com>
2026-06-29 12:05:45 +0900

    通信エラー用のAPIError enumを追加

--- github-repository-search/APIError.swift
+++ github-repository-search/APIError.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+/// completion に渡す Error の種類を enum で定義する。
+enum APIError: Error {
+    /// データが取得できなかった場合のエラー。
+    case noData
+    /// URL の作成に失敗した場合のエラー。
+    case invalidURL
+}
