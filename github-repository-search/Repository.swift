bb412bcda5243fe43550fb4822da56f28784e1df
cocona15531 <issei.ueda@zozo.com>
2026-06-29 10:48:14 +0900

    SearchResult構造体を作成

--- github-repository-search/Repository.swift
+++ github-repository-search/Repository.swift
@@ -7,5 +7,12 @@
 
 import Foundation
 
+/// GitHub のリポジトリ検索 API のレスポンスを表す構造体。
+///
+/// Decodable プロトコルに準拠しているので、JSON から自動的にデコードできる。
+struct SearchResponse: Decodable {
+    /// レポジトリ情報を含んだ配列。
+    let items: [Repository]
+}
 struct Repository: Decodable {
   }
