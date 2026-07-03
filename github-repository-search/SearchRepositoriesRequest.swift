fa482a0a8b87a56240f47fbdcec943d0d3023d04
cocona15531 <issei.ueda@zozo.com>
2026-07-03 18:41:21 +0900

    SearchRepositoriesRequestを追加

---
+++ github-repository-search/SearchRepositoriesRequest.swift
@@ -0,0 +1,19 @@
+//
+//  SearchRepositoriesRequest.swift
+//  github-repository-search
+//
+//  Created by Issei Ueda on 2026/07/03.
+//
+
+import Foundation
+
+/// GitHub のリポジトリ検索 API のリクエストを表す構造体。
+struct SearchRepositoriesRequest: RequestType {
+    static let path = "/search/repositories"
+    static let method: HTTPMethod = .get
+
+    let query: String
+    var queryItems: [URLQueryItem] {
+        [URLQueryItem(name: "q", value: query)]
+    }
+}
