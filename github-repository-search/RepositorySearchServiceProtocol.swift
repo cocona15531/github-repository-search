4c6962a99aac196f210be71ba08933efc2863951
cocona15531 <issei.ueda@zozo.com>
2026-07-14 12:51:30 +0900

    RepositorySearchServiceProtocolを追加

---
+++ github-repository-search/RepositorySearchServiceProtocol.swift
@@ -0,0 +1,21 @@
+//
+//  RepositorySearchServiceProtocol.swift
+//  github-repository-search
+//
+//  Created by Issei Ueda on 2026/07/14.
+//
+
+import Foundation
+
+/// リポジトリ検索の API 呼び出しを抽象化するプロトコル。
+///
+/// APIClient から必要なメソッドを切り出し、Repository の依存を最小化する。
+protocol RepositorySearchServiceProtocol {
+    func searchRepositories(_ request: SearchRepositoriesRequest) async throws(APIError) -> SearchResponse
+}
+
+extension APIClient: RepositorySearchServiceProtocol {
+    func searchRepositories(_ request: SearchRepositoriesRequest) async throws(APIError) -> SearchResponse {
+        try await send(request)
+    }
+}
