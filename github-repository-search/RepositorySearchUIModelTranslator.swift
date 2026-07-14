2f42d42687205d7cc4e5ce2a86a1074708db8148
cocona15531 <issei.ueda@zozo.com>
2026-07-14 21:06:04 +0900

    RepositorySearchUIModelTranslatorを追加

---
+++ github-repository-search/RepositorySearchUIModelTranslator.swift
@@ -0,0 +1,20 @@
+//
+//  RepositorySearchUIModelTranslator.swift
+//  github-repository-search
+//
+//  Created by Issei Ueda on 2026/07/14.
+//
+
+import Foundation
+
+/// GitHubRepository を RepositoryRowUIModel に変換する。
+enum RepositorySearchUIModelTranslator {
+    static func translate(from repository: GitHubRepository) -> RepositoryRowUIModel {
+        RepositoryRowUIModel(
+            id: repository.id,
+            name: repository.name,
+            description: repository.description ?? "説明はありません",
+            starCountText: "★ \(repository.stargazersCount)"
+        )
+    }
+}
