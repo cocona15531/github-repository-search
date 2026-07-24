b830db669059d406b0ab15e6b411dd366d625196
cocona15531 <issei.ueda@zozo.com>
2026-07-24 09:09:19 +0900

    RepositoryDetailViewControllerのクラス定義を追加

--- github-repository-search/ViewController/RepositoryDetail/RepositoryDetailViewController.swift
+++ github-repository-search/ViewController/RepositoryDetail/RepositoryDetailViewController.swift
@@ -5,4 +5,23 @@
 //  Created by Issei Ueda on 2026/07/23.
 //
 
-import Foundation
+import UIKit
+
+/// リポジトリの詳細情報を表示する画面。
+///
+/// レイアウトと表示内容の反映は後続のコミットで追加する。
+final class RepositoryDetailViewController: UIViewController {
+    private let viewModel: RepositoryDetailViewModel
+
+    init(viewModel: RepositoryDetailViewModel) {
+        self.viewModel = viewModel
+        super.init()
+    }
+
+    required init?(coder: NSCoder) { fatalError() }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
